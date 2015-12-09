52e8af3f8ff03fdbdf1acc4c266d6143ea6858ad
Day Davis Waterbury <day.waterbury@gmail.com>
2015-12-08 18:57:47 -0800

    Failing specs for Hash.functify metamonkeypatch

--- spec/functify_spec.rb
+++ spec/functify_spec.rb
@@ -3,3 +3,36 @@ require "rspec"
 # FUNCTIFY v0.0.1
 # Given a hash, adds a function for each key in the hash which returns the value
 # for that key at the time it was functified. E.g. {:a => 1}.functify.a == 1
+describe Hash do
+  describe "#functify" do
+    describe "creates a function for each key in the hash," do
+      [2,3,5].each do |n|
+        context "so {:a => #{n}}.functify.a" do
+          it "returns #{n}" do
+            expect({:a => n}.functify.a).to eq(n)
+            expect{{:a => n}.functify.b}.to_raise MethodNotFound
+          end
+        end
+      end
+    end
+    it "modifies self" do
+      h = {:a => 1}
+      h.functify
+      expect(h.a).to eq(1)
+    end
+    it "does not modify every Hash object" do
+      {:a => 1}.functify
+      expect({:a => 1}.a).to_raise MethodNotFound
+    end
+    it "is still a hash" do
+      expect({:a => 1}.functify[:a]).to eq(1)
+    end
+    it "is frozen for functions, but not hash values" do
+      h = {:a => 1}
+      h.functify
+      h[:a] = 2
+      expect(h.a).to eq(1)
+      expect(h[:a]).to eq(2)
+    end
+  end
+end
